0851fbc2f0c5f963ca52b3f86dac2764ce61fd80
lvxianlei <704894244@qq.com>
2020-08-25 22:48:59 +0800

    删除dist目录，添加忽略

--- dist/Route/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Router = require("koa-router");
-const Login_1 = require("./Login");
-const router = new Router();
-router.use('/login', Login_1.default.routes(), router.allowedMethods());
-exports.default = router;
-//# sourceMappingURL=index.js.map
